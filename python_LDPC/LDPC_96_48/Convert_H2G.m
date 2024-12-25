34e6bf008f82d68a1e5e60cde4a6ea7b3a04c103
Chougigi <chougigi.ee12@nycu.edu.tw>
2024-12-25 19:37:20 +0800

    remove file
    remove file

--- python_LDPC/LDPC_96_48/Convert_H2G.m
+++
@@ -1,8 +0,0 @@
-%%
-filename = "H_96_48_Mat.txt";
-H = readMatrixFromFileByLine(filename);
-filename = "H_96_48_G.txt";
-[N, K, M_ori, M_sys, sysG, sysH, G, PCM ]= f_GaussJordanE_gf2_HPIform_TFR(H);
-writeMatrixToFile(G,filename)
-
-
