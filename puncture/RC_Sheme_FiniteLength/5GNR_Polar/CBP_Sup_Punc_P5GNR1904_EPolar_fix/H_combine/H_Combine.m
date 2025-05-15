87882bf3d0d502813bf99dc9e80c05c4979e5df4
Chougigi <chougigi.ee12@nycu.edu.tw>
2025-05-15 15:17:15 +0800

    new_file

--- puncture/RC_Sheme_FiniteLength/5GNR_Polar/CBP_Sup_Punc_P5GNR1904_EPolar_fix/H_combine/H_Combine.m
+++ puncture/RC_Sheme_FiniteLength/5GNR_Polar/CBP_Sup_Punc_P5GNR1904_EPolar_fix/H_combine/H_Combine.m
@@ -5,7 +5,7 @@ close all;
 %%
 PayloadH_file = 'C:\Users\USER\Desktop\LDPC\PCM\H_1904_616_1288_BG1_Z28_fix.txt'; % payload data
 ExtraH_file = 'C:\Users\USER\Desktop\LDPC\PCM\Polar_16_fix.txt'; % extra data
-H_combine_file = 'PCM_5GNR1904_Polar16_RCSheme.txt';
+H_combine_file = 'PCM_5GNR1904_Polar16_RCSheme_fix.txt';
 puncture_position_bits_file = ['Punc_Pos_', H_combine_file];
 Z  = 28;
 
@@ -55,46 +55,46 @@ fclose(fileID);
 
 
 % 設定要輸出的 Excel 檔名
-filename = 'matrix_output.xlsx';
-
-% 將矩陣寫入 Excel（使用 writematrix，從 A1 開始）
-writematrix(H_combine, filename, 'Sheet', 1, 'Range', 'A1');
-
-% 啟用 ActiveX 控制 Excel（僅限 Windows）
-excel = actxserver('Excel.Application');
-excel.Visible = false;  % 設成 true 可以看到 Excel 操作
-
-% 打開檔案
-workbook = excel.Workbooks.Open(fullfile(pwd, filename));
-sheet = workbook.Sheets.Item(1);
-
-% 遍歷矩陣，將值為 1 的格子設為填色（黃色）
-[rows, cols] = size(H_combine);
-for i = 1:rows
-    for j = 1:cols
-        if H_combine(i, j) == 1
-            cell = sheet.Range(getExcelRange(j, i));  % Excel 是列字母+行數字
-            cell.Interior.Color = 65535;  % 黃色
-        end
-    end
-end
-
-% 儲存並關閉
-workbook.Save();
-workbook.Close(false);
-excel.Quit();
-delete(excel);
-
-disp('矩陣已寫入並標記完成！');
-
-% --- 輔助函式：數字轉 A1 格式（e.g., j=1,i=1 變 A1）
-function ref = getExcelRange(col, row)
-    letters = '';
-    while col > 0
-        rem = mod(col-1, 26);
-        letters = [char(65+rem), letters];
-        col = floor((col-1)/26);
-    end
-    ref = [letters, num2str(row)];
-end
+% filename = 'matrix_output.xlsx';
+% 
+% % 將矩陣寫入 Excel（使用 writematrix，從 A1 開始）
+% writematrix(H_combine, filename, 'Sheet', 1, 'Range', 'A1');
+% 
+% % 啟用 ActiveX 控制 Excel（僅限 Windows）
+% excel = actxserver('Excel.Application');
+% excel.Visible = false;  % 設成 true 可以看到 Excel 操作
+% 
+% % 打開檔案
+% workbook = excel.Workbooks.Open(fullfile(pwd, filename));
+% sheet = workbook.Sheets.Item(1);
+% 
+% % 遍歷矩陣，將值為 1 的格子設為填色（黃色）
+% [rows, cols] = size(H_combine);
+% for i = 1:rows
+%     for j = 1:cols
+%         if H_combine(i, j) == 1
+%             cell = sheet.Range(getExcelRange(j, i));  % Excel 是列字母+行數字
+%             cell.Interior.Color = 65535;  % 黃色
+%         end
+%     end
+% end
+% 
+% % 儲存並關閉
+% workbook.Save();
+% workbook.Close(false);
+% excel.Quit();
+% delete(excel);
+% 
+% disp('矩陣已寫入並標記完成！');
+% 
+% % --- 輔助函式：數字轉 A1 格式（e.g., j=1,i=1 變 A1）
+% function ref = getExcelRange(col, row)
+%     letters = '';
+%     while col > 0
+%         rem = mod(col-1, 26);
+%         letters = [char(65+rem), letters];
+%         col = floor((col-1)/26);
+%     end
+%     ref = [letters, num2str(row)];
+% end
 
