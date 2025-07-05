38a54755f15923955d143fdd9aa29731ba894290
Chougigi <chougigi.ee12@nycu.edu.tw>
2025-07-05 10:57:35 +0800

    new

---
+++ Throughput/Combine/PayLoad1008_Extra(BCH15)_origin/Combine_H_throughput.cpp
@@ -0,0 +1,345 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<string>
+#include "random_number_generator.h"
+#include "UseFuction.h"
+#include "BP_function.h"
+
+using namespace std;
+
+void Read_File_H(struct parity_check *H,string& file_name_in);
+vector<vector<bool>> Read_File_G(string& file_name);
+vector<int> Read_punc_pos(string file,int  extra_nums);
+vector<double> ProcessCodeWord_ReturnLLR(vector<vector<bool>>& G,struct parity_check H,bool Encode_Flag,double sigma,vector<int>& CodeWord);
+vector<double> ProcessXorCodeWord_ReturnLLR(vector<vector<bool>>& Payload_G,struct parity_check Payload_H,bool Payload_Encode_Flag,double sigma,vector<int>& CodeWordA,vector<int>& CodeWordB,vector<int>& punc_map,vector<int>& xor_map,vector<vector<bool>>& Extra_G,struct parity_check H);
+
+int main(int argc,char* argv[]){
+    double total_frame_count = 1e6;
+    if(argc < 2){
+        cout << "** Error ---- No file in \n" ; 
+    }
+    string H_combine_file = argv[1];
+    string Payload_PCM_file = argv[2];
+    string Extra_PCM_file = argv[3];
+    string  Xor_Position_file = argv[4];
+    int  xor_vn_nums = atoi(argv[5]);
+    string PayLoad_G_file = argv[6]==string("None")?"":argv[6];
+    bool PayLoad_Flag = PayLoad_G_file==""?false:true;
+    string Extra_G_file = argv[7]==string("None")?"":argv[7];
+    bool Extra_Flag = Extra_G_file==""?false:true;
+    int iteration_limit = atoi(argv[8]);
+    double SNR_min = atof(argv[9]);
+    double SNR_max = atof(argv[10]);
+    double SNR_ratio = atof(argv[11]);
+    int iteration_open = atoi(argv[12]); // Open_Iteration
+    string puncture_pos_file = argv[13];
+    int punc_vn_nums = atoi(argv[14]);
+    string out_file = argv[15];
+    cout << "##############################" << "\n";
+    cout << "H_combine_file : " << H_combine_file << "\n";
+    cout << "Payload PCM file : " << Payload_PCM_file << "\n";
+    cout << "Extra PCM file : " << Extra_PCM_file << "\n";
+    cout << "puncture_pos_file : " << puncture_pos_file << "\n";
+    cout << "Payload G file : " << PayLoad_G_file << "\n";
+    cout << "PayLoad_Flag : " << PayLoad_Flag << "\n";
+    cout << "Extra G file : " << Extra_G_file << "\n";
+    cout << "Extra_Flag : " << Extra_Flag << "\n";
+    cout << "Iteration_limit : " << iteration_limit << "\n";
+    cout << "SNR_min : " << SNR_min << "\n";
+    cout << "SNR_max : " << SNR_max << "\n";
+    cout << "SNR_ratio : " << SNR_ratio << "\n";
+    cout << "iteration_open : " << iteration_open << "\n";
+    cout << "Xor_Position_file : " << Xor_Position_file << "\n";
+    cout << "out_file : " << out_file << "\n";
+    cout << "##############################" << "\n";
+    // 
+
+    // define H_combine
+    struct parity_check H;
+    Read_File_H(&H,H_combine_file);
+    cout << "H_combine file read success!!" << endl;
+    /*-------------------------------------------*/
+    // define PayLoad_H
+    struct parity_check PayLoad_H;
+    Read_File_H(&PayLoad_H,Payload_PCM_file);
+    cout << "PayLoad_H file read success!!" << endl;
+    /*-------------------------------------------*/
+    // define Extra_H
+    struct parity_check Extra_H;
+    Read_File_H(&Extra_H,Extra_PCM_file);
+    cout << "Extra_H file read success!!" << endl;
+    /*-------------------------------------------*/
+    // Read Puncture pos
+    vector<int> punc_map; // RV0
+    punc_map = Read_punc_pos(puncture_pos_file,punc_vn_nums);
+    cout << "Puncture position file read success !!" << endl;
+    // Read Xor pos
+    vector<int> xor_map; // RV1
+    xor_map = Read_punc_pos(Xor_Position_file,xor_vn_nums);
+    cout << "Xor position file read success !!" << endl;
+    /*-------------------------------------------*/
+     // define PayLoad_G
+    vector<vector<bool>> PayLoad_G;
+    if(PayLoad_Flag){
+        PayLoad_G = Read_File_G(PayLoad_G_file);
+        cout << "PayLoad_G file read success!!" << endl;
+    }
+    // define Extra_G
+    vector<vector<bool>> Extra_G;
+    if(Extra_Flag){
+        Extra_G = Read_File_G(Extra_G_file);
+        cout << "Extra_G file read success!!" << endl;
+    }
+
+    // create extra out csv
+    ofstream outfile(out_file);
+    if (!outfile.is_open()) {
+        cout << "Output file: " << out_file  << " cannot be opened." << endl;
+		exit(1);
+    }
+    outfile << "SNR" << ", " << "Throughput" << "\n";
+    /*-------------------------------------------*/
+    double SNR = SNR_min;
+    double code_rate = 0.5; // throught
+    
+    double total_bits = 0;
+    double correct_bits = 0;
+    while(SNR<SNR_max){
+        double frame_count = 0;
+        int it=0;
+        // cout << "SNR : " << SNR << "\n";
+        for(double frame_count=0;frame_count<total_frame_count;frame_count++){
+            /* Process A with RV0*/
+            // decode A version with RV0
+            vector<int> A_CodeWord(PayLoad_H.n,0);
+            vector<double> A_LLR = ProcessCodeWord_ReturnLLR(PayLoad_G,PayLoad_H,PayLoad_Flag,1.0,A_CodeWord); // sigmas =1
+            for(int i=0;i<punc_map.size();i++) A_LLR[punc_map[i]] = 0; // punc 7 position(RV1)
+
+            /* -----------------*/
+            bool decode_flag_A = BP_for_Payload(PayLoad_G,PayLoad_H,PayLoad_Flag,SNR,code_rate,iteration_limit,A_LLR);
+            total_bits += (PayLoad_H.n-punc_vn_nums); // total bits
+            if(decode_flag_A==true){
+                correct_bits += PayLoad_H.n - PayLoad_H.m; // info bits
+                continue;
+            }
+                    
+            // decode B version RV0 + A RV1
+            vector<int> B_CodeWord(H.n,0);
+            vector<double> B_LLR(H.n,0);
+            if(PayLoad_Flag){ // 處理 Payload CodeWord 是zero 還是Encode
+                vector<bool> info_bits((PayLoad_G.size()));
+                for(int i=0;i<PayLoad_G.size();i++){
+                    info_bits[i] = random_generation()>0.5?1:0;
+                }
+                B_CodeWord = Vector_Dot_Matrix_Int(info_bits,PayLoad_G);
+                for(int i=0;i<B_CodeWord.size();i++){
+                    B_CodeWord[i] = B_CodeWord[i]%2;
+                }
+            }
+            // do A parital CodeWord to Encode(BCH)
+            vector<bool> info_bits(punc_map.size(),0);
+            // cout << "punc_map size : " << punc_map.size() << endl;
+            for(int i=0;i<punc_map.size();i++){
+                int vn_pos = punc_map[i];
+                info_bits[i] = A_CodeWord[vn_pos] % 2;
+            }
+            vector<int> Extra_CodeWord = Vector_Dot_Matrix_Int(info_bits,Extra_G);
+            for(int i=0;i<Extra_CodeWord.size();i++) Extra_CodeWord[i] = Extra_CodeWord[i] % 2;
+            // do Xor 
+            for(int i=0;i<xor_map.size();i++){
+                int xor_vn = xor_map[i];
+                B_CodeWord[xor_vn] = B_CodeWord[xor_vn] ^ Extra_CodeWord[i];
+            }
+            for(int i=0;i<PayLoad_H.n;i++){ // Count LLR
+                // BPSK -> {0->1}、{1->-1}
+                // double receiver_codeword=(-2*transmit_codeword[i]+1)/sigma + gasdev(); // power up
+                double receive_value=(double)(-2*B_CodeWord[i]+1) + 1*gasdev(); // fixed power
+                B_LLR[i]=2*receive_value/pow(1,2); 
+            }
+            vector<double> copy_B_LLR = B_LLR; // copy for later use
+            for(int i=0;i<punc_map.size();i++){
+                int punc_vn = punc_map[i]; //  RV1
+                B_LLR[punc_vn] = 0; // RV1
+            }
+            // assign LLR to correct position
+            for(int i=0;i<xor_map.size();i++){
+                int base_pos = PayLoad_H.n + Extra_H.n + i; // 1018 ~ 1027
+                B_LLR[base_pos] = copy_B_LLR[xor_map[i]]; // RV1
+                B_LLR[xor_map[i]] = 0; // combine H
+            }
+            bool decode_flag_B = BP_for_CombineH(PayLoad_G,PayLoad_H,SNR,code_rate,iteration_limit,iteration_open,B_LLR,Extra_G,Extra_H,H);
+            total_bits += PayLoad_H.n;
+            if(decode_flag_B == true){
+                correct_bits += PayLoad_H.n - PayLoad_H.m; // info bits
+                for(int i=Extra_H.n-punc_map.size();i<Extra_H.n;i++){
+                    int punc_vn = punc_map[i]; // A RV1
+                    A_LLR[punc_vn] = B_CodeWord[punc_vn]==0?B_LLR[punc_vn]:-1*B_LLR[punc_vn]; // A RV1
+                }
+                bool decode_flag_A = BP_for_Payload(PayLoad_G,PayLoad_H,PayLoad_Flag,SNR,code_rate,iteration_limit,A_LLR);
+                if(decode_flag_A==true){
+                    correct_bits += PayLoad_H.n - PayLoad_H.m; // info bits                    
+                }
+            }
+        }
+        double throughput = (correct_bits/total_bits);
+        outfile << SNR << ", " << throughput << "\n";
+        cout << "SNR : " << SNR << " | " << "Throughput : " << throughput << "% |\n ";
+    }
+}
+
+vector<double> ProcessCodeWord_ReturnLLR(vector<vector<bool>>& G,struct parity_check H,bool Encode_Flag,double sigma,vector<int>& CodeWord){
+    vector<double> LLR(H.n,0); 
+    if(Encode_Flag){ // 處理 Payload CodeWord 是zero 還是Encode
+        vector<bool> info_bits((G.size()));
+        for(int i=0;i<G.size();i++){
+            info_bits[i] = random_generation()>0.5?1:0;
+        }
+        CodeWord = Vector_Dot_Matrix_Int(info_bits,G);
+        for(int i=0;i<CodeWord.size();i++){
+            CodeWord[i] = CodeWord[i]%2;
+        }
+    }
+    for(int i=0;i<H.n;i++){ // Count LLR
+        // BPSK -> {0->1}、{1->-1}
+        // double receiver_codeword=(-2*transmit_codeword[i]+1)/sigma + gasdev(); // power up
+        double receive_value=(double)(-2*CodeWord[i]+1) + sigma*gasdev(); // fixed power
+        LLR[i]=2*receive_value/pow(sigma,2); 
+    }
+    return LLR;
+}
+
+
+void Read_File_H(struct parity_check *H,string& file_name_in){
+    ifstream file_in(file_name_in);
+    if(file_in.fail()){
+        cout << "** Error ----- " << file_name_in << " can't open !! \n" ;
+        exit(1);
+    }
+    
+    // struct parity_check *H=(struct parity_check*)malloc(sizeof(parity_check));
+    file_in >> H->n >> H->m >> H->max_col_degree >> H->max_row_degree;
+    
+    cout << "Parity_Check_Matrix size  : " << H->m << " x " << H->n << endl;
+    cout << "max_col_degree : " << H->max_col_degree << " | max_row_degree : " << H->max_row_degree << "\n";
+    H->max_col_arr=(int*)malloc(sizeof(int)*H->n);
+    H->max_row_arr=(int*)malloc(sizeof(int)*H->m);
+    // cout << "Each col 1's number : ";
+    for(int i=0;i<H->n;i++){
+        file_in >> H->max_col_arr[i];
+        if(H->max_col_arr[i] > H->max_col_degree){
+            cout << "** Error ----- " << file_name_in << " max_col_array is Error , Please Check!!" << "\n";
+            exit(1);
+        }
+        // cout << H->max_col_arr[i] << " ";
+    }
+    // cout << endl << "Each row 1's number : ";
+    for(int i=0;i<H->m;i++){
+        file_in >> H->max_row_arr[i];
+        if(H->max_row_arr[i] > H->max_row_degree){
+            cout << "** Error ----- " << file_name_in << " max_row_array is Error , Please Check!!" << "\n";
+            exit(1);
+        }
+        // cout << H->max_row_arr[i] << " ";
+    }
+    // cout << endl;
+    H->CN_2_VN_pos=(int**)malloc(sizeof(int*)*H->n);
+    H->VN_2_CN_pos=(int**)malloc(sizeof(int*)*H->m);
+    for(int i=0;i<H->n;i++){
+        H->CN_2_VN_pos[i]=(int*)malloc(H->max_col_degree*sizeof(int));
+    }
+    for(int i=0;i<H->m;i++){
+        H->VN_2_CN_pos[i]=(int*)malloc(H->max_row_degree*sizeof(int));
+    }
+    for(int i=0;i<H->n;i++){
+        int pos;
+        for(int j=0;j<H->max_col_degree;j++){
+            file_in >> pos;
+            H->CN_2_VN_pos[i][j]=pos-1;
+        }
+        if(H->max_col_arr[i]!=H->max_col_degree){
+            H->CN_2_VN_pos[i] = (int*)realloc(H->CN_2_VN_pos[i],sizeof(int)*H->max_col_arr[i]);
+        }
+    }
+    for(int i=0;i<H->m;i++){
+        int pos;
+        for(int j=0;j<H->max_row_degree;j++){
+            file_in >> pos;
+            H->VN_2_CN_pos[i][j]=pos-1;
+        }
+        if(H->max_row_arr[i]!=H->max_row_degree){
+            H->VN_2_CN_pos[i] = (int*)realloc(H->VN_2_CN_pos[i],sizeof(int)*H->max_row_arr[i]);
+        }
+    }
+    file_in.close();
+    return;
+}
+
+vector<int> Read_punc_pos(string file,int extra_nums){
+    ifstream punc_file(file);
+    vector<int> punc_map;
+    for(int i=0;i<extra_nums;i++){
+        int pos;
+        punc_file >> pos;
+        punc_map.push_back(pos-1);
+    }
+    punc_file.close();
+    return punc_map;
+}
+
+vector<vector<bool>> Read_File_G(string& file_name){
+    ifstream file(file_name);
+    string line;
+    vector<string> line_split;
+    int idx=0,n,k,max_col;
+    vector<vector<bool>> G;
+    vector<int> max_col_map;
+    while (getline(file, line)) {
+        line = strip(line);
+        // cout << line << endl;
+        line_split = split(line,' ');
+        if(idx==0){
+            if(line_split.size()==2){
+                n = stoi(line_split[0]);
+                k = stoi(line_split[1]);
+                G = vector<vector<bool>>(k,vector<bool>(n,0));
+                cout << "G - n : " << n << " | k : " << k << "\n";
+            }else{
+                cerr << "First line(n,k) : " << line << " is not n,k !! \n";
+                exit(1);
+            }
+        }else if(idx==1){
+            if(line_split.size()==1){
+                max_col = stoi(line_split[0]);
+                cout << "max col : "  << max_col << "\n";
+            }else{
+                cerr << "Second line(maxcol) : " << line << " is not max_col !! \n" ;
+                exit(1);
+            }
+        }else if(idx==2){
+            if(line_split.size()==n){
+                for(int i=0;i<line_split.size();i++){
+                    max_col_map.push_back(stoi(line_split[i]));
+                }
+            }
+            else{
+                cerr << "Third line(each col 1's number) : " << line << " is not equal G matrix colsize  !! \n";
+                exit(1);
+            }
+        }
+        else{
+            if(line_split.size()==max_col_map[idx-3]){
+                for(string& s:line_split){
+                    int pos = stoi(s);
+                    if(pos!=0) G[pos-1][idx-3]=true; // 1
+                }
+            }else{
+                cerr << "Each Col 1 postion line : [" << line << "] postion length is not equal to " << max_col << " | length : " << line_split.size() << "\n";
+                exit(1); 
+            }
+        }
+        idx++;
+    }
+    file.close();
+    return G;
+}
---
+++ Throughput/Combine/PayLoad1008_Extra(BCH15)_origin/H_combine/test.m
@@ -0,0 +1,75 @@
+%%
+clc;
+clear all;
+close all;
+%%
+H1_file = 'C:\Users\USER\Desktop\LDPC\PCM\PEGReg504x1008.txt'; % payload data
+H2_file = 'C:\Users\USER\Desktop\LDPC\PCM\BCH_15_7.txt'; % extra data
+H_combine_file = 'PCM_Punc_kSR_Punc7.txt';
+puncture_position_bits_file = ['Pos_', H_combine_file];
+RV1_punc_pos = "Pos_RV1_puncpos.txt";
+H1 = readHFromFileByLine(H1_file);
+H2 = readHFromFileByLine(H2_file);
+[H1_r,H1_c] = size(H1);
+[H2_r,H2_c] = size(H2);
+% H_combine = [  H1     zero1   zero2 
+%               zero3     H2    zero4
+%               punc_p  punc_e    I
+%             ] 
+zero1 = zeros([H1_r H2_c]);
+zero2 = zeros([H1_r H2_c]);
+zero3 = zeros([H2_r H1_c]);
+zero4 = zeros([H2_r H2_c]);
+H_combine = [[H1,zero1,zero2];[zero3,H2,zero4]];
+
+Already_Punc_VNs = [];
+% Already_Punc_VNs = 1:(2*Z);
+% puncture_bits_num = H2_c + length(Already_Punc_VNs);
+puncture_bits_num = 7 + H2_c;
+% puncture_bits_num = H2_c*3;
+punc_pos_bits = Rate_Compatible_Punctured_With_Short_Block_Lengths(H1,puncture_bits_num,Already_Punc_VNs);
+punc_pos_bits = setdiff(punc_pos_bits,Already_Punc_VNs);
+xor_vn = punc_pos_bits(7+1:end);
+RV1_punc = punc_pos_bits(1:7);
+%% check  punc_pos_bits 是否都是 1-SR
+for Punc_VN=xor_vn 
+    Punc_SCNs = transpose(find(H1(:,Punc_VN)==1));
+    SCNs_num = 0;
+    for SCN=Punc_SCNs
+        Nei_SCN_VNs = setdiff(find(H1(SCN,:)==1),Punc_VN);
+        if ~any(ismember(punc_pos_bits,Nei_SCN_VNs))
+            SCNs_num  = SCNs_num + 1;
+        end
+    end
+    if SCNs_num==0
+        disp("find no 1-SR punc vn !!\n");
+    end
+end
+%%
+punc_payload_mat = zeros([H2_c,H1_c]);
+for r=1:H2_c
+    punc_vn = xor_vn(r);
+    punc_payload_mat(r,punc_vn) = 1;
+end
+
+I = eye(H2_c);
+H_combine = [H_combine;punc_payload_mat,I,I];
+
+% write H_combine to PCM 
+writePCM(H_combine,H_combine_file);
+
+% write payload data with puncture bits position 
+fileID = fopen(puncture_position_bits_file, 'w');
+for pos=xor_vn
+    fprintf(fileID, '%d ', pos);
+end
+fprintf(fileID, '\n');
+fclose(fileID);
+
+% write payload data with puncture bits position 
+fileID = fopen(RV1_punc_pos, 'w');
+for pos=RV1_punc
+    fprintf(fileID, '%d ', pos);
+end
+fprintf(fileID, '\n');
+fclose(fileID);
