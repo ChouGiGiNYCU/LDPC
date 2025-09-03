e93c40c36d3684ee6e5f558a8ce4a9038005296d
Chougigi <chougigi.ee12@nycu.edu.tw>
2025-09-03 15:53:21 +0800

    new G matrix

--- Convert_H2G_src/Convert_H2G.m
+++ Convert_H2G_src/Convert_H2G.m
@@ -1,8 +1,8 @@
 clear all;
 clc;
 % H_matrix = [0 1 1 0 0 0;1 0 1 1 0 0;1 0 0 1 1 0;0 0 0 0 1 1];
-file_name = "C:\Users\USER\Desktop\LDPC\PCM\rep_1_3.txt";
-G_filename = "C:\Users\USER\Desktop\LDPC\GM\rep_1_3_G.txt";
+file_name = "C:\Users\USER\Desktop\LDPC\PCM\H_32_16_CCSDS.txt";
+G_filename = "C:\Users\USER\Desktop\LDPC\GM\H_32_16_CCSDS_G.txt";
 H_matrix = readHFromFileByLine(file_name);
 [N, K, M_ori, M_sys, sysG, sysH, G, PCM ]= f_GaussJordanE_gf2_HPIform_TFR(H_matrix);
 writeMatrixToFile(G,G_filename);
